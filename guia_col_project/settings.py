30a99fe3e468e2e3c76cca479e0b092e2d0ae078
jcardenasc93 <jcardenasc93@gmail.com>
2019-08-19 16:36:32 -0500

    Configura variables de entorno para DB

--- guia_col_project/settings.py
+++ guia_col_project/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'guia_col_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': os.environ.get('DB_NAME', ''),
-    #    'USER': os.environ.get('DB_USER', ''),
-    #    'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-    #    'HOST': os.environ.get('DB_HOSTNAME', ''),
-    #    'PORT': os.environ.get('DB_PORT', ''),
-    #}
-    
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'guia_col',
-        'USER': 'postgres',
-        'PASSWORD': 'jc1095jc',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOSTNAME', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
+    
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'guia_col',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'jc1095jc',
+    #    'HOST': 'localhost',
+    #    'PORT': '',
+    #}
     
 }
 
