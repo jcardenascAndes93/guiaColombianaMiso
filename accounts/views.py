ade309d95d44f4e4396fd46eed45921a63353620
jcardenasc93 <jcardenasc93@gmail.com>
2019-09-01 16:04:28 -0500

    Correccion del view para el registro de usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -11,30 +11,12 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .serializers import UserSerializer
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
-
-
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
-
-
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
+    # authentication_classes = (TokenAuthentication,)
     # permission_classes = (IsAuthenticated,)
-    permission_classes = (IsAdminUser,)
+    # permission_classes = (IsAdminUser,)
--- guia_col_project/settings.py
+++ guia_col_project/settings.py
@@ -154,4 +154,5 @@ django_heroku.settings(locals())
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+
 }
