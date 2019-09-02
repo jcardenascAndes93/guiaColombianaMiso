0ef011525d30e310ffd9d972b8bdb8cc85a0bd69
jcardenasc93 <jcardenasc93@gmail.com>
2019-09-01 19:30:01 -0500

    Modificacion de views para mostrar guias en el home

--- guicolapp/views.py
+++ guicolapp/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
+from django.core import serializers
+from django.http import HttpResponse
 from guicolapp.models import Guia
 from .models import Tour
 
@@ -14,12 +16,7 @@ from .models import Tour
 @csrf_exempt
 def guias(request):
     guias = Guia.objects.all()
-    paginator = Paginator(guias, 8)
-    page = request.GET.get('page')
-
-    guias = paginator.get_page(page)  # < New in 2.0!
-
-    return render(request, 'guicolapp/guias.html', {'guias': guias})
+    return HttpResponse(serializers.serialize("json", guias))
 
 
 @csrf_exempt
@@ -29,6 +26,7 @@ def detalleGuias(request):
         guia = Guia.objects.filter(full_name=name)
         print(name)
     return render(request, "guicolapp/detalleGuias.html", {'name': name})
+
 
 @csrf_exempt
 def index(request):
